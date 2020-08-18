625f8cf36533629d9ef0062ae7f147b017a48f21
Vicente Manriquez <vmanriquezcortes@gmail.com>
2020-08-17 20:06:26 -0400

    Feature/hu 5 (#1)
    * Funcion para subir archivos creada
    
    * [UPDATE] add serializers errors

--- repo_project/apps/archivos/views/archivos_download.py
+++ repo_project/apps/archivos/views/archivos_download.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from ..serializers import ArchivoSerializer
 from django.http import JsonResponse
 
+from rest_framework import status
 from rest_framework.decorators import api_view
-
+from rest_framework.response import Response
 
 from ..models import Archivo
 
---
+++ repo_project/apps/archivos/views/archivos_upload.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from ..serializers import ArchivoSerializer
+from django.http import JsonResponse
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from ..models import Archivo
+
+
+@api_view(["POST"])
+def archivo_upload(request):
+
+    serializer = ArchivoSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        print(serializer.errors)
+        print(request.data)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
