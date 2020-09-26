cfa6434ead4e669e7b04483a9b12b127bbdde42f
Marogareh <maximiliano.aravena@usm.cl>
2020-09-26 05:03:25 -0300

    ahora si actualizado con develop

--- repo_project/apps/archivos/admin.py
+++ repo_project/apps/archivos/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 from .models import Archivo
 from ramos.models import Ramo
+from .views.archivos_aceptar import archivos_aceptar, archivos_rechazar
 # Register your models here.
+
+
+class ArchivoAdmin(admin.ModelAdmin):
+    actions = [archivos_aceptar, archivos_rechazar]
+
 admin.site.register(Archivo)
 admin.site.register(Ramo)
+admin.site.register(ArchivoAdmin)
