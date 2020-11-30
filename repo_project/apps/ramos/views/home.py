e8c49b7c33fb9cfa02b7007fa2e6de7478811b84
Vicente Manriquez <vmanriquezcortes@gmail.com>
2020-11-30 16:22:56 -0300

    fix: fix sidebar to show semesters

--- repo_project/apps/ramos/views/home.py
+++ repo_project/apps/ramos/views/home.py
@@ -5,8 +5,17 @@ from ramos.models import Ramo
 
 def home(request):
 	all_ramos = Ramo.objects.all()
+	ramo_by_semestre = {}
+	for ramo in all_ramos:
+		semestre = str(ramo.semestre)
+		if semestre not in ramo_by_semestre.keys():
+			ramo_by_semestre[semestre] = []
+		ramo_by_semestre[semestre].append(ramo.sigla)
+	ramo_by_semestre = list(ramo_by_semestre.items())
+	ramo_by_semestre.sort()
 	context = {
-		'ramos': all_ramos
+		'ramos':all_ramos,
+		'ramo_by_semestre':ramo_by_semestre
 	}
 	print(context)
 	return render(request, "home.html", context)
